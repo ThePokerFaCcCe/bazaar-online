7ea4b7dc5af35c7c512df368271a61eb2f7bd0ce
iamPedram1 <pedram.pps757@gmail.com>
2022-07-21 14:37:00 +0430

    Implementing NavBar

--- Client/pages/_app.tsx
+++ Client/pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from "next/app";
+import NavBar from "../component/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "antd/dist/antd.css";
 import "../styles/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
