7977d917848f884e16eca20e6a0943eeb5e047cc
ThePokerFaCcCe <ThePokerFa3e@gmail.com>
2022-07-29 05:01:31 +0430

    :wrench: Initial Configurations

--- Server/Src/BazaarOnline.API/Program.cs
+++ Server/Src/BazaarOnline.API/Program.cs
@@ -1,11 +1,42 @@
+using BazaarOnline.Infra.Data.Contexts;
+using FluentValidation.AspNetCore;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// DotNetEnv
+DotNetEnv.Env.Load();
 
 builder.Services.AddControllers();
+
+// DB
+builder.Services.AddDbContext<BazaarDbContext>(options =>
+    options.UseSqlServer(DotNetEnv.Env.GetString("CONNECTION_STRING")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("V1", new OpenApiInfo
+    {
+        Version = "V1",
+        Title = "Bazaar API",
+        Description = "Main API Documentation of Bazaar API",
+        License = new OpenApiLicense { Name = "MIT" },
+        Contact = new OpenApiContact
+        {
+            Email = "matin.khaleghi.nezhad@gmail.com",
+            Name = "Matin Khaleghi"
+        }
+    });
+});
+// Fluent Validation
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddFluentValidationRulesToSwagger();
+
 
 var app = builder.Build();
 
@@ -13,11 +44,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/V1/swagger.json", "V1");
+    });
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
