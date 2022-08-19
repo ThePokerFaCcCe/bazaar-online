b0465dbf7bb42eaf11c3871ca20a103cb339cb3a
ThePokerFaCcCe <ThePokerFa3e@gmail.com>
2022-08-19 19:43:25 +0430

    :sparkles: Add GUID generators

--- Server/Src/BazaarOnline.Application/Generators/StringGenerator.cs
+++ Server/Src/BazaarOnline.Application/Generators/StringGenerator.cs
@@ -8,5 +8,18 @@ namespace BazaarOnline.Application.Generators
         {
             return RandomNumberGenerator.GetInt32(100000, 999999).ToString();
         }
+        public static string GenerateUniqueCode()
+        {
+            return Guid.NewGuid().ToString();
+        }
+        public static string GenerateUniqueCodeWithoutDash()
+        {
+            return Guid.NewGuid().ToString().Replace("-", "");
+        }
+
+        public static string GenerateShortKey()
+        {
+            return Guid.NewGuid().ToString().Substring(5);
+        }
     }
 }
