7fcef6f7fec7410385c7c9dea336ead6c8b80215
ThePokerFaCcCe <ThePokerFa3e@gmail.com>
2022-08-20 18:29:00 +0430

    :hammer: Improve Example Seeder

--- Server/Src/BazaarOnline.Infra.Data/Seeds/ExampleSeeder.cs
+++ Server/Src/BazaarOnline.Infra.Data/Seeds/ExampleSeeder.cs
@@ -1,5 +1,7 @@
 using BazaarOnline.Domain.Entities.Categories;
+using BazaarOnline.Domain.Entities.Users;
 using BazaarOnline.Infra.Data.Contexts;
+using BazaarOnline.Infra.Data.Seeds.DefaultDatas;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,6 +9,8 @@ namespace BazaarOnline.Infra.Data.Seeds
 {
     public static class ExampleSeeder
     {
+        private static string ExamplesDir { get; set; }
+        private static BazaarDbContext Context { get; set; }
         /// <summary>
         /// Seed Example data to database
         /// </summary>
@@ -16,40 +20,71 @@ namespace BazaarOnline.Infra.Data.Seeds
         {
             var serverDir = Directory.GetParent(Directory.GetCurrentDirectory()).ToString();
             var seedsDir = Path.Combine(serverDir, "BazaarOnline.Infra.Data/Seeds");
-            var examplesDir = Path.Combine(seedsDir, "ExampleDatas");
 
-            var context = services.GetRequiredService<BazaarDbContext>();
-            context.Users.RemoveRange(context.Users);
-            context.SaveChanges();
-            using (var transaction = context.Database.BeginTransaction())
+            ExamplesDir = Path.Combine(seedsDir, "ExampleDatas");
+            Context = services.GetRequiredService<BazaarDbContext>();
+
+            using (var transaction = Context.Database.BeginTransaction())
             {
                 if (forceRecreate)
                 {
-                    context.Categories.RemoveRange(context.Categories);
+                    Context.Categories.RemoveRange(Context.Categories);
+                    Context.Users.RemoveRange(Context.Users);
 
-                    context.SaveChanges();
+                    Context.SaveChanges();
                 }
 
-                if (context.Categories.Count() == 0)
-                {
-                    context.Categories.AddRange(
-                        SeedHelper.LoadFromJson<Category>(Path.Combine(examplesDir, "CategorySeed.json"))
-                    );
-                }
-
-                SetIdentityInsert<Category>(context, true);
-                context.SaveChanges();
-                SetIdentityInsert<Category>(context, false);
+                SeedUsers();
+                SeedCategories();
 
                 transaction.Commit();
             }
         }
 
-        private static void SetIdentityInsert<T>(DbContext context, bool enable)
+        private static void SeedCategories()
         {
-            var entityType = context.Model.FindEntityType(typeof(T));
+            if (Context.Categories.Count() == 0)
+            {
+                Context.Categories.AddRange(
+                    SeedHelper.LoadFromJson<Category>(Path.Combine(ExamplesDir, "CategorySeed.json"))
+                );
+            }
+
+            SaveChangesWithIdentityInsert<Category>();
+        }
+
+        private static void SeedUsers()
+        {
+            Context.Users.Add(new User
+            {
+                Id = 1,
+                FirstName = "string",
+                LastName = "string",
+                Email = "user@example.com",
+                PhoneNumber = "09191587842",
+                Password = "string",
+                IsActive = true,
+                UserRoles = new List<UserRole>{
+                        new UserRole{
+                            RoleId=DefaultRoles.Owner.Id
+                        }
+                    }
+            });
+            SaveChangesWithIdentityInsert<User>();
+        }
+
+        private static void SaveChangesWithIdentityInsert<T>()
+        {
+            SetIdentityInsert<T>(Context, true);
+            Context.SaveChanges();
+            SetIdentityInsert<T>(Context, false);
+        }
+
+        private static void SetIdentityInsert<T>(DbContext Context, bool enable)
+        {
+            var entityType = Context.Model.FindEntityType(typeof(T));
             var value = enable ? "ON" : "OFF";
-            context.Database.ExecuteSqlRaw(
+            Context.Database.ExecuteSqlRaw(
                 $"SET IDENTITY_INSERT {entityType.GetSchema()}.{entityType.GetTableName()} {value}");
         }
     }
