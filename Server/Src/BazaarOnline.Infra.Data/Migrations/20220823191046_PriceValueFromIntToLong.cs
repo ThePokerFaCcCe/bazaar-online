cd7a451a5e3434a55c8571c715c7256f2dfb02e1
ThePokerFaCcCe <ThePokerFa3e@gmail.com>
2022-08-24 00:22:21 +0430

    :card_file_box: Change Price value from int to long

---
+++ Server/Src/BazaarOnline.Infra.Data/Migrations/20220823191046_PriceValueFromIntToLong.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BazaarOnline.Infra.Data.Migrations
+{
+    public partial class PriceValueFromIntToLong : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Value",
+                table: "AdvertiesementPrice",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Value",
+                table: "AdvertiesementPrice",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+    }
+}
