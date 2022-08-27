37624d4925dc3ead87f4621def0ca8a1129f7c97
ThePokerFaCcCe <ThePokerFa3e@gmail.com>
2022-08-27 13:56:21 +0430

    :card_file_box: Change activecode structure

--- Server/Src/BazaarOnline.Domain/Entities/Users/ActiveCode.cs
+++ Server/Src/BazaarOnline.Domain/Entities/Users/ActiveCode.cs
@@ -4,10 +4,14 @@ namespace BazaarOnline.Domain.Entities.Users
     {
         public int Id { get; set; }
 
-        public string Email { get; set; }
+        public int UserId { get; set; }
 
         public string Code { get; set; }
 
         public DateTime ExpireDate { get; set; }
+
+        #region Relations
+        public User User { get; set; }
+        #endregion
     }
 }
